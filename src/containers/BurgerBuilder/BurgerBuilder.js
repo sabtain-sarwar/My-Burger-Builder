9700c3a504dd0f255f703b1ac7d7ee8cbbd67650
sabtain-sarwar <sabtain964@gmail.com>
2020-05-10 01:31:39 +0500

    Created Burger component and its CSS

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React , { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
     render () {
         return (
             // i want to return 2 adjacent elements at the end
             <Auxiliary>
-                <div>Burger with all the ingredients</div>
+                <Burger />
                 <div>Build controls.The area where i can add or remove ingredients</div>
             </Auxiliary>
 
