db8455cf069815c8696ca1c72aafcfd325d45250
sabtain-sarwar <sabtain964@gmail.com>
2020-05-10 16:56:09 +0500

    Outputting Burger  ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,21 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 const Burger = (props) => {
+
+    console.log(props.ingredients);
+    console.log(Object.keys(props.ingredients));
+    
+    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map(( _ , i ) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+    });
+    console.log(transformedIngredients);
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,23 @@ import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients : {
+            salad : 1 , 
+            bacon : 1 , 
+            cheese : 2 , 
+            meat : 2
+        }
+    };
+
     render () {
+        // y this line runs 2 times
+         console.log(this.state.ingredients['salad']); 
         return (
             // i want to return 2 adjacent elements at the end
             <Auxiliary>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build controls.The area where i can add or remove ingredients</div>
             </Auxiliary>
 
