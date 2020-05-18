a8acceff85312efef9247f8b38a214cfe1f86139
sabtain-sarwar <sabtain964@gmail.com>
2020-05-18 16:12:02 +0500

    Created Responsive SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const Layout = ( props ) => {
     return (
         <Auxiliary>
             <Toolbar />
+            <SideDrawer />
             <main className={classes.Content}>
                 {props.children}
             </main>
