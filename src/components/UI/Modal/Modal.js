d5db4317b8f8116cd57ff51ba9a7ade6276acf97
sabtain-sarwar <sabtain964@gmail.com>
2020-05-20 02:27:44 +0500

    Improving Performance

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Auxiliary from '../../../hoc/Auxiliary';
 import BackDrop from '../BackDrop/BackDrop';
 
-const Modal = (props) => (
-    <Auxiliary>
-        <BackDrop show={props.show} clicked={props.modalClosed}/>
-        <div 
-            className={classes.Modal} 
-            style={{
-                transform : props.show ? 'translateY(0)' : 'translateY(-100vh)' , 
-                opacity : props.show ? '1' : '0'
-            }}>
-            {/* children can be our own component , can be a text , a paragraph that's totally up to us. We can pass anything in there */}
-            {props.children}
-        </div>
-    </Auxiliary>
-);
+class Modal extends Component {
+
+
+    shouldComponentUpdate = (nextProps, nextState) => {
+        return nextProps.show !== this.props.show
+    };
+
+    componentWillUpdate = () => {
+        console.log("[Modal] willUpdate ");
+    };
+
+
+     render () {
+         return (
+            <Auxiliary>
+                <BackDrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div 
+                    className={classes.Modal} 
+                    style={{
+                        transform : this.props.show ? 'translateY(0)' : 'translateY(-100vh)' , 
+                        opacity : this.props.show ? '1' : '0'
+                    }}>
+                    {/* children can be our own component , can be a text , a paragraph that's totally up to us. We can pass anything in there */}
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+         );
+     }
+}
+
 
 export default Modal;
