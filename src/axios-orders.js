61cb21c8c63336c75096d3187f5fa5f4b0180a42
sabtain-sarwar <sabtain964@gmail.com>
2020-06-04 13:14:14 +0500

    Sending a POST request

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -4,4 +4,4 @@ const instance = axios.create({
     baseURL : 'https://react-my-burger-264b5.firebaseio.com/'
 });
 
-export default axios;
+export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 
 const INGREDIENT_PRICES = {
@@ -93,7 +94,25 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        alert('You can continue');
+        // alert('You can continue');
+        const order = {
+            ingredients : this.state.ingredients , 
+            price : this.state.totalPrice ,
+            customer : {
+                name : 'Sabtain Sarwar' , 
+                address : {
+                    street : 'Green town # 2 , strret # 8' , 
+                    zipCode : '4321' , 
+                    country : 'Germany'
+                } , 
+                email : 'sabtain964@gmail.com'
+            } ,
+            deliveryMethod : 'fastest'
+        };
+
+        axios.post('/orders.json' , order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     };
 
     render () {
