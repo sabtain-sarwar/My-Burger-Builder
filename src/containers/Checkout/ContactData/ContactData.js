b8f2dab1ea08b1a3710f9eaaa2b9b7954cbf602f
sabtain-sarwar <sabtain964@gmail.com>
2020-06-17 10:43:22 +0500

    Navigating to ContactData Component

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,35 @@
+import React , { Component } from 'react';
+
+import Button from "../../../components/UI/Button/Button";
+import classes from './ContactData.module.css';
+
+class ContactData extends Component {
+
+
+    state = {
+        name : '' ,
+        email : '' ,
+        address : {
+            street : '' ,
+            postalCode : ''
+        }
+    }
+
+    render () {
+        return (
+            <div className={classes.ContactData}> 
+                <h4>Enter your contact Data</h4>
+                <form>
+                    <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+                    <input className={classes.Input} type="email" name="name" placeholder="Your Mail" />
+                    <input className={classes.Input} type="text" name="name" placeholder="Street" />
+                    <input className={classes.Input} type="text" name="name" placeholder="Postal Code" />
+                    <Button btnType="Success">ORDER</Button>
+                </form>
+            </div>
+        ); 
+    }
+
+} 
+
+export default ContactData;
