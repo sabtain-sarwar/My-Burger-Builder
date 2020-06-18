0f3d97fba493d4ee08cb2dd41958d9db5fd7f49b
sabtain-sarwar <sabtain964@gmail.com>
2020-06-18 10:42:45 +0500

    Setup a  JS config for the Form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -10,12 +10,58 @@ class ContactData extends Component {
 
 
     state = {
-        name : '' ,
-        email : '' ,
-        address : {
-            street : '' ,
-            postalCode : ''
-        } , 
+        orderForm : {
+            name : {
+                elementType : 'input' ,
+                elementConfig : {
+                    type : 'text' ,
+                    placeholder : 'Your name'
+                } ,
+                value : ''
+            } , 
+            street : {
+                elementType : 'input' ,
+                elementConfig : {
+                    type : 'text' ,
+                    placeholder : 'Street'
+                } ,
+                value : ''
+            } ,  
+            zipCode : {
+                elementType : 'input' ,
+                elementConfig : {
+                    type : 'text' ,
+                    placeholder : 'ZIP Code'
+                } ,
+                value : ''
+            } , 
+            country : {
+                elementType : 'input' ,
+                elementConfig : {
+                    type : 'text' ,
+                    placeholder : 'Country'
+                } ,
+                value : ''
+            } ,  
+            email : {
+                elementType : 'input' ,
+                elementConfig : {
+                    type : 'email' ,
+                    placeholder : 'Your E-Mail'
+                } ,
+                value : ''
+            } , 
+            deliveryMethod : {
+                elementType : 'select' ,
+                elementConfig : {
+                   options : [
+                       {value : "fastest" , displayValue : 'Fastest'}
+                       {value : "cheapest" , displayValue : 'Cheapest'}
+                   ]
+                } ,
+                value : ''
+            } 
+        } ,
         loading : false
     }
 
@@ -25,16 +71,7 @@ class ContactData extends Component {
         const order = {
             ingredients : this.props.ingredients , 
             price : this.props.price ,
-            customer : {
-                name : 'Sabtain Sarwar' , 
-                address : {
-                    street : 'Green town # 2 , strret # 8' , 
-                    zipCode : '4321' , 
-                    country : 'Germany'
-                } , 
-                email : 'sabtain964@gmail.com'
-            } ,
-            deliveryMethod : 'fastest'
+            
         };
 
         axios.post('/orders.json' , order)
@@ -52,7 +89,7 @@ class ContactData extends Component {
     render () {
         let form = (
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+                <Input elementType="" elementConfig="" value="" />
                 <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
                 <Input inputtype="input" type="text" name="street" placeholder="Street" />
                 <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
