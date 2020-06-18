221f5fa81e375ee389843439af5b8f061124f0e8
sabtain-sarwar <sabtain964@gmail.com>
2020-06-18 12:11:34 +0500

    Dynamically created Inputs based on JS config

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -55,7 +55,7 @@ class ContactData extends Component {
                 elementType : 'select' ,
                 elementConfig : {
                    options : [
-                       {value : "fastest" , displayValue : 'Fastest'}
+                       {value : "fastest" , displayValue : 'Fastest'} ,
                        {value : "cheapest" , displayValue : 'Cheapest'}
                    ]
                 } ,
@@ -87,12 +87,22 @@ class ContactData extends Component {
     }
 
     render () {
+        const formElementArray = [];
+        for (let key in this.state.orderForm) {
+            formElementArray.push({
+                id : key , 
+                config : this.state.orderForm[key]
+            });
+        }
         let form = (
             <form>
-                <Input elementType="" elementConfig="" value="" />
-                <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
-                <Input inputtype="input" type="text" name="street" placeholder="Street" />
-                <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+                {formElementArray.map(formElement => (
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value} />
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
