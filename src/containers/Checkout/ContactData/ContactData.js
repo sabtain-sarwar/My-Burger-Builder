2364e2bc46036aa83b81618cd7554d651f4cfb92
sabtain-sarwar <sabtain964@gmail.com>
2020-06-22 10:09:55 +0500

    Adding custom form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -17,7 +17,11 @@ class ContactData extends Component {
                     type : 'text' ,
                     placeholder : 'Your name'
                 } ,
-                value : ''
+                value : '' ,
+                validation : {
+                    required : true
+                } , 
+                valid : false
             } , 
             street : {
                 elementType : 'input' ,
@@ -25,7 +29,11 @@ class ContactData extends Component {
                     type : 'text' ,
                     placeholder : 'Street'
                 } ,
-                value : ''
+                value : '' ,
+                validation : {
+                    required : true
+                }, 
+                valid : false
             } ,  
             zipCode : {
                 elementType : 'input' ,
@@ -33,7 +41,13 @@ class ContactData extends Component {
                     type : 'text' ,
                     placeholder : 'ZIP Code'
                 } ,
-                value : ''
+                value : '' ,
+                validation : {
+                    required : true ,
+                    minLength : 5 ,
+                    maxLength : 5
+                }, 
+                valid : false
             } , 
             country : {
                 elementType : 'input' ,
@@ -41,7 +55,11 @@ class ContactData extends Component {
                     type : 'text' ,
                     placeholder : 'Country'
                 } ,
-                value : ''
+                value : '' ,
+                validation : {
+                    required : true
+                }, 
+                valid : false
             } ,  
             email : {
                 elementType : 'input' ,
@@ -49,7 +67,11 @@ class ContactData extends Component {
                     type : 'email' ,
                     placeholder : 'Your E-Mail'
                 } ,
-                value : ''
+                value : '' ,
+                validation : {
+                    required : true
+                }, 
+                valid : false
             } , 
             deliveryMethod : {
                 elementType : 'select' ,
@@ -59,7 +81,7 @@ class ContactData extends Component {
                        {value : "cheapest" , displayValue : 'Cheapest'}
                    ]
                 } ,
-                value : ''
+                value : '' 
             } 
         } ,
         loading : false
@@ -92,6 +114,27 @@ class ContactData extends Component {
             });
     }
 
+
+    // return true or false determining whether this is valid or not
+    checkValidity = (value , rules) => {
+        let isValid = false;
+        if (rules.required) {
+            // i want to set isValid equal to the value comparison. So isValid should be equal if it is not equal to an
+            // empty string.And we use trim there to remove any white spaces
+            isValid = value.trim() !== '';
+            // Now isValid is equal to true/false dpending on the check if the trimmed value is unequal to empty string
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength ;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength ;
+        }
+        return isValid;
+    }
+
     inputChangedHandler = (event , inputIdentifier) => {
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -100,7 +143,9 @@ class ContactData extends Component {
             ...updatedOrderForm[inputIdentifier]
         };
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value , updatedFormElement.validation);
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement);
         this.setState({orderForm : updatedOrderForm});
     }
 
