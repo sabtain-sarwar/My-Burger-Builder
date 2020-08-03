815ff2cb513642f848ff4f2a5d7fc8700748d457
sabtain-sarwar <sabtain964@gmail.com>
2020-08-03 17:36:09 +0500

    Redirect to improve UX

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React , { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route , Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -48,20 +48,30 @@ class Checkout extends Component {
     };
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary 
-                    // ingredients={this.state.ingredients} 
-                    ingredients={this.props.ings} 
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler} />
+        let summary = <Redirect to="/"  />
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary 
+                        // ingredients={this.state.ingredients} 
+                        ingredients={this.props.ings} 
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler} 
+                    />
                     <Route 
                         path={this.props.match.path + '/contact-data'} 
                         // render={(props) => <ContactData ingredients={this.state.ingredients} price={this.state.price} {...props} />} 
-                       component={ContactData} 
+                        component={ContactData} 
                     />
-            </div>
-        );
+                </div>
+            );
+        }
+        return summary;
+        //(
+            // <div> due to the div that we have added to the summary
+                // {summary}
+            // </div>
+        //);
     }
 }
 
